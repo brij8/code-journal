c65a83b829cd52c5da17e32e177afd3b1e807320
Brighitte Johnson <bmuses@gmail.com>
2022-02-15 17:20:50 -0800

    listen for input events, update img src to photo-url input_05

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* global data */
 /* exported data */
+var $photoUrl = document.getElementById('photo-url');
+
+$photoUrl.addEventListener('input', imgPreview);
+
+function imgPreview(event) {
+  document.getElementById('preview').src = document.getElementById('photo-url').value;
+  if (document.getElementById('photo-url').value === '') {
+    document.getElementById('preview').src = 'images/placeholder-image-square.jpg';
+  }
+}
