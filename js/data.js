4ac31edc55c6ac6be3893f08509f462d8f42771b
Brighitte Johnson <bmuses@gmail.com>
2022-02-17 17:42:20 -0800

    issue02 tasks 5 & 6, function to make dom tree & loop runs that function for all entries

--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var journalJSON = localStorage.getItem('javascript-local-storage');
+
+if (journalJSON !== null) {
+  data = JSON.parse(journalJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+});
--- js/main.js
+++ js/main.js
@@ -15,30 +15,74 @@ function imgPreview(event) {
 
 function subFunc(event) {
   event.preventDefault();
-  var journalJSON = localStorage.getItem('javascript-local-storage');
-  let dataModel;
-  if (journalJSON !== null) {
-    dataModel = JSON.parse(journalJSON);
-  } else {
-    dataModel = {
-      nextEntryId: 0,
-      entries: []
-    };
-  }
-
   var entry = {
     title: event.target[0].value,
     image: event.target[1].value,
     note: event.target[2].value,
-    entryId: dataModel.nextEntryId
+    entryId: data.nextEntryId
   };
-
-  dataModel.entries.unshift(entry);
-  dataModel.nextEntryId++;
-
-  var dataModelJSON = JSON.stringify(dataModel);
-  localStorage.setItem('javascript-local-storage', dataModelJSON);
-
+  data.nextEntryId++;
+  data.entries.unshift(entry);
   document.getElementById('preview').src = 'images/placeholder-image-square.jpg';
   event.target.reset();
+}
+
+function entryToDOM(event) {
+
+  var $entryLI = document.createElement('li');
+  $entryLI.className = 'entry-list-item';
+
+  var $row = document.createElement('row');
+  $row.className = 'row';
+  $entryLI.appendChild($row);
+
+  var $columnHalf01 = document.createElement('div');
+  $columnHalf01.className = 'column-half';
+  $row.appendChild($columnHalf01);
+
+  var $entryImg = document.createElement('div');
+  $entryImg.className = 'entry-image';
+  $columnHalf01.appendChild($entryImg);
+
+  var $imgUrl = document.createElement('img');
+  $imgUrl.setAttribute('src', event.image);
+  $imgUrl.id = 'entryImg';
+  $entryImg.appendChild($imgUrl);
+
+  var $columnHalf02 = document.createElement('div');
+  $columnHalf02.className = 'column-half';
+  $row.appendChild($columnHalf02);
+
+  var $entryDiv = document.createElement('div');
+  $entryDiv.className = 'entry-div';
+  $columnHalf02.appendChild($entryDiv);
+
+  var $entryLabel = document.createElement('div');
+  $entryLabel.className = 'entry-label';
+  $entryDiv.appendChild($entryLabel);
+
+  var $entryTitle = document.createElement('label');
+  $entryTitle.setAttribute('for', 'entry-title');
+  $entryTitle.innerHTML = event.title;
+  $entryLabel.appendChild($entryTitle);
+
+  var $entryText = document.createElement('div');
+  $entryText.className = 'entry-text';
+  $entryDiv.appendChild($entryText);
+
+  var $entryCopy = document.createElement('p');
+  $entryCopy.innerHTML = event.note;
+  $entryText.appendChild($entryCopy);
+
+  return $entryLI;
+
+}
+
+window.addEventListener('DOMContentLoaded', appendLi);
+
+function appendLi(event) {
+  var $entryList = document.querySelector('.entry-list');
+  for (let i = 0; i < data.entries.length; i++) {
+    $entryList.appendChild(entryToDOM(data.entries[i]));
+  }
 }
